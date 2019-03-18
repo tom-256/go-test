f78d671e0d4013db9740c8341ea32c45d5592bed
tom-256 <t11a.256.512.1024@gmail.com>
2019-03-18 18:59:59 +0900

    use struct

---
+++ structs/shape.go
@@ -0,0 +1,12 @@
+package shape
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
