e31e6ef20e889eaec15d6be07e255b57e392bd90
tom-256 <t11a.256.512.1024@gmail.com>
2019-03-29 19:51:06 +0900

    v5

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,6 +13,14 @@ func TestWallet(t *testing.T){
 		}
 	}
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+
+		if err == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -35,9 +43,6 @@ func TestWallet(t *testing.T){
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 }
