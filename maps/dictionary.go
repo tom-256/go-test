c47adbbba6e74c36154335388fb7199d8430759f
tom-256 <t11a.256.512.1024@gmail.com>
2019-03-31 15:11:58 +0900

    report that the word is not in the dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary)Search(dic map[string]string, word string) string {
-	return dic[word]
+func (d Dictionary)Search(word string) (string, error) {
+	definition, found := d[word]
+	if !found {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition,nil
 }
