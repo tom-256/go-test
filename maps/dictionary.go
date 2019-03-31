2d242c15fb5d790f7c655698df13f381be894897
tom-256 <t11a.256.512.1024@gmail.com>
2019-03-31 14:54:51 +0900

    using a custom type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,7 @@
 package main
 
-func Search(dic map[string]string, word string) string {
+type Dictionary map[string]string
+
+func (d Dictionary)Search(dic map[string]string, word string) string {
 	return dic[word]
 }
